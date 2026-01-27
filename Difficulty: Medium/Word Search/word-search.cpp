f726edb0c4349f177f6ee8bca732cd29797e3a2a
Sahil Chhillar <12212237@nitkkr.ac.in>
2026-01-27 19:20:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Word Search/word-search.cpp
+++ Difficulty: Medium/Word Search/word-search.cpp
@@ -1,33 +1,33 @@
 class Solution {
   public:
     vector<pair<int,int>> dir = {{-1,0},{0,1},{1,0},{0,-1}};
-    bool f(int r,int c, int idx,vector<vector<char>> &mat, string &word,int m,int n){
+    bool f(int r,int c, int idx,vector<vector<char>> &mat, string &word,vector<vector<bool>>& visitedCell,int m,int n){
         if(idx >= word.size() || mat[r][c] != word[idx]) return false;
-        
         if(idx == word.size()-1) return true;
         
-        char temp = mat[r][c];
-        mat[r][c] = '#';
-        
+        visitedCell[r][c] = true;
         for(auto& d : dir){
             int nr = r + d.first;
             int nc = c + d.second;
-            if((nr < m && nr >= 0) && (nc < n && nc >= 0) && mat[nr][nc] != '#'){
-                if(f(nr,nc,idx+1,mat,word,m,n)) return true;
+            if((nr < m && nr >= 0) && (nc < n && nc >= 0) && !visitedCell[nr][nc]){
+                if(f(nr,nc,idx+1,mat,word,visitedCell,m,n)) return true;
             }
         }
-        
-        mat[r][c] = temp;
+        visitedCell[r][c] = false;
         return false;
     }
     
     bool isWordExist(vector<vector<char>> &mat, string &word) {
         int m = mat.size();
         int n = mat[0].size();
+        vector<vector<bool>> visitedCell(m,vector<bool>(n,false));
+        
         for(int r = 0; r <m;r++){
             for(int c = 0 ; c < n ;c++){
                 if(mat[r][c] == word[0]){
-                    if(f(r,c,0,mat,word,m,n)) return true;
+                    visitedCell[r][c] = true;
+                    if(f(r,c,0,mat,word,visitedCell,m,n)) return true;
+                    visitedCell[r][c] = false;
                 }
             }
         }
